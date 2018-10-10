70807bb8b161096a38e77385b56fb170baf5b4f1
LeRomainMalin <romain.remaud@viacesi.fr>
2018-10-10 16:23:01 +0200

    Tépatrebo

--- website-skeleton/src/Controller/OrangeController.php
+++ website-skeleton/src/Controller/OrangeController.php
@@ -8,12 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class OrangeController extends AbstractController
 {
     /**
-     * @Route("/query", name="query")
+     * @Route("/", name="query")
      */
     public function query()
     {
         $apiKey = 'bb09d609';
-        $movies = "Avengers";
+        $movies = "Alien";
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s=' . $movies . '&apikey=' . $apiKey);
@@ -32,7 +32,33 @@ class OrangeController extends AbstractController
 
         );
     }
+	
+	 /**
+     * @Route("/film/{movies}", name="premier")
+     */
+    public function premier($movies)
+    {
+        $apiKey = 'bb09d609';
+        //$movies = "Nightmare";
 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s=' . $movies . '&apikey=' . $apiKey);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $resultat_curl = curl_exec($ch);
+
+        $json = json_decode($resultat_curl);
+
+
+        return $this->render('orange/index.html.twig',
+            array(
+                'query' => $movies,
+                'movies' => $json->Search,
+            )
+
+        );
+    }
+	
 
     /**
      * @Route("/film/{movies}", name="film"
@@ -56,6 +82,31 @@ class OrangeController extends AbstractController
             array(
                 'query' => $movies,
                 'movies' => $json->Search,
+            )
+
+        );
+    }
+	
+	  /**
+     * @Route("/fiche/{detail}", name="fiche"
+     * )
+     */
+    public function fiche($detail)
+    {
+        $apiKey = 'bb09d609';
+        //$movies = "Avengers";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?i=' . $detail . '&apikey=' . $apiKey);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $resultat_curl = curl_exec($ch);
+
+        $json = json_decode($resultat_curl);
+
+        return $this->render('deux.html.twig',
+            array(
+                'movie' => $json,
             )
 
         );
